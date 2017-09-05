b3707d1ae6c7ae8b24c94188cf04044c12a6e66b
Thomas Waters <twaters117@gmail.com>
2017-09-05 15:09:50 -0400

    More modularization and starting 2D Paper js

---
+++ js/rendering/colorSchemes/DefaultColors.js
@@ -0,0 +1,51 @@
+// Color Assignments
+DefaultColors = {};
+
+// Others
+DefaultColors.black = '#000000';
+DefaultColors.white = '#FFFFFF';
+DefaultColors.gray = '#A0A0A0';
+DefaultColors.lightGray = '#E0E0E0';
+DefaultColors.magenta = '#FF00FF';
+DefaultColors.bad = '#FF6666'
+DefaultColors.good = '#33FF33'
+
+// Genaric Biomes
+DefaultColors.water = '#74BBFD';
+DefaultColors.land = '#99CC99';
+DefaultColors.mountain = '#CCFFFF';
+DefaultColors.coast = '#EBE0C0';
+DefaultColors.ocean = '#4864DB';
+
+// Plate Boundaries
+DefaultColors.convergent = '#FF0000';
+DefaultColors.transform = '#00FF00';
+DefaultColors.divergent = '#0000FF';
+
+// Geo Provinces
+DefaultColors.oceanCrust = '#99CCFF';
+DefaultColors.craton = '#FF99FF';
+DefaultColors.orogen = '#66FFB2';
+DefaultColors.basin = '#9999FF';
+
+// Temperatures
+DefaultColors.hot = '#FF9933';
+DefaultColors.cold = '#66B2FF';
+
+// Biomes
+// ocean already decalred
+DefaultColors.marsh = '#37B596';
+DefaultColors.ice = '#BDFFFF';
+DefaultColors.beach = '#F8EFBD';
+DefaultColors.snow = '#FFF3FC';
+DefaultColors.tundra ='#B9D271';
+DefaultColors.bare = '#B5AD8B';
+DefaultColors.taiga = '#A8C95F';
+DefaultColors.shrubland = '#A3CA7C';
+DefaultColors['temperate desert'] = '#E8DF91';
+DefaultColors['temperate rainforest'] = '#45B33B';
+DefaultColors['temperate deciduous'] = '#7BC16E';
+DefaultColors.grassland = '#ADD37D';
+DefaultColors['tropical rainforest'] = '#23A336';
+DefaultColors['tropical seasonal forest'] = '#57BB57';
+DefaultColors['subtropic desert'] = '#F4EEA4';
